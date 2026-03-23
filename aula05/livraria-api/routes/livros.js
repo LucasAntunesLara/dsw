6549a7e2fd21ee93d75867a80c6bfa9a23cdff60
Lucas Antunes <lucasantuneslara06@gmail.com>
2026-03-23 17:31:21 -0300

    Exercício API de livraria completo

--- aula05/livraria-api/routes/livros.js
+++ aula05/livraria-api/routes/livros.js
@@ -2,19 +2,25 @@ const express = require('express')
 const router = express.Router()
 const livroController = require('../controllers/livroController')
 
+// Rota GET /livros - lista todos os livros
 router.get('/', livroController.listar)
 
-router.post('/', livroController.adicionar)
-
+// Rota GET /livros/completo - lista livros com editoras
 router.get('/completo', livroController.listarComEditoras)
 
-module.exports = router
+// Rota GET /livros/:id - busca livro por ID
+router.get('/:id', livroController.buscarPorId)
 
-exports.listarComEditoras = callback => {
-  const sql = `
- SELECT livros.*, editoras.nome AS editora
- FROM livros
- JOIN editoras ON livros.editora_id = editoras.id
- `
-  connection.query(sql, callback)
-}
+// Rota GET /livros/:id - busca livro por ID
+router.get('/:id', livroController.buscarPorId)
+
+// Rota POST /livros - adiciona novo livro
+router.post('/', livroController.adicionar)
+
+// Rota PUT /livros/:id - atualiza um livro
+router.put('/:id', livroController.atualizar)
+
+// Rota DELETE /livros/:id - remove um livro
+router.delete('/:id', livroController.deletar)
+
+module.exports = router
