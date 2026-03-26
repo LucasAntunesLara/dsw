51d2b167ad95b8f3819a86804980661edd6e932b
LUCAS LARA <lucas.060406@bento.local>
2026-03-26 20:00:23 -0300

    Aula 26/03 - NoSQL e MongoDB

---
+++ aula06/pedidos-api/routes/pedidoRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const pedidoController = require('../controllers/pedidoController')
+
+// Rotas para pedido
+router.post('/', pedidoController.criarPedido)
+router.get('/', pedidoController.listarPedidos)
+router.get('/buscar', pedidoController.buscarPedidos) // 🔍 busca com filtros
+
+module.exports = router
